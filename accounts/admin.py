d2093f94902e71b11539b81cdf871b6a1a403a7a
Jeffer-UAO <jeffer443@gmail.com>
2023-07-12 16:37:46 -0500

    Implementacion usuarios

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,7 +9,7 @@ from django.utils.html import format_html
 #     pass
 
 class AccountAdmin(UserAdmin):
-    list_display = ('email', 'first_name', 'username', 'last_login', 'date_joined', 'is_active')
+    list_display = ('email', 'first_name', 'last_name', 'username', 'last_login', 'date_joined', 'is_active')
     list_display_links = ('email', 'first_name', 'last_name')
     readonly_fields = ('last_login', 'date_joined')
     ordering = ('-date_joined',)
