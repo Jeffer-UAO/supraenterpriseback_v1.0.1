9c342ee05bd6a44b1a7c149dd5405488b8568236
JefferDevop <somadesarrollo@gmail.com>
2024-06-05 06:06:48 -0500

    return Videos manager

--- videos/api/router.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import DefaultRouter
-from videos.api.views import VideosApiViewSet
-
-router_videos = DefaultRouter()
-
-
-router_videos.register(
-    prefix='videos', basename='videos', viewset=VideosApiViewSet
-)
--- videos/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from videos.models import Video
-
-
-
-class VideosSerializer(ModelSerializer):
-    class Meta:
-        model = Video
-        fields = ["video_url", "name"]
